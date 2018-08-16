9fba904a07f22b0761265220b88d082565fc3835
aver117 <42426016+aver117@users.noreply.github.com>
2018-08-15 20:02:20 -0700

    Create NodeClass.java

---
+++ SinglyLinkedLists/NodeClass.java
@@ -0,0 +1,12 @@
+/**
+ * This is the static inner class used in all of the linked list problems in this section.
+ */
+
+static class Node {
+  public int data;
+  public Node next; // Object references default to null if uninitialized
+
+  public Node(int data) {
+    this.data = data;
+  }
+}
